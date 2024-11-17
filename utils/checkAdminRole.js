b453f20f3098b3a937ab61997d201c6a3b2551d0
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-17 14:54:27 +0500

    candidate controller is added

---
+++ utils/checkAdminRole.js
@@ -0,0 +1,14 @@
+const User = require("../models/userModel");
+
+const checkAdminRole = async (userID) => {
+  try {
+    const user = await User.findById(userID);
+    if (user.role === "admin") {
+      return true;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+module.exports = checkAdminRole;
